ea364e18bdd5d9e6d427a5f67a5ad5f6991e7ebf
Blake Branvold <37115648+Underdoneboar4@users.noreply.github.com>
2022-11-22 10:10:18 -0600

    add files via upload

---
+++ backend-testing/src/main/java/com/example/backendtesting/BackendTestingApplication.java
@@ -0,0 +1,14 @@
+package com.example.backendtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.
+
+@SpringBootApplication
+public class BackendTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTestingApplication.class, args);
+	}
+
+}
